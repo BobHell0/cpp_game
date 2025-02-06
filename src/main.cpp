22abc1003c7b937fae90f7ab30879f4b71607244
BobHell0 <anujan.cenan@gmail.com>
2025-02-06 19:06:46 +1100

    took tutorial code and adapted it to create a cover page

---
+++ src/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "./constants.h"
+#include "./headerFiles/MenuState.hpp"
+
+struct gameObject {
+    float x;
+    float y;
+    float width;
+    float height;
+};
+
+
+bool initialise_window(SDL_Window **window, SDL_Renderer **renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    *window = SDL_CreateWindow(
+        "Following a tutorial",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (window == NULL) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    std::cout << "Window created successfully" << std::endl;
+
+    *renderer = SDL_CreateRenderer(*window, -1, 0);
+    if (renderer == NULL) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    std::cout << "Renderer created successfully" << std::endl;
+    return true;
+}
+
+void process_input(bool *gameIsRunning) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                *gameIsRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    *gameIsRunning = false;
+                }
+                break;
+        }
+    }
+}
+
+
+void update(int *lastFrameTime) {
+    // waste time time until we reach the frame target time
+    // int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - lastFrameTime);
+    // if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+    //     SDL_Delay(time_to_wait);
+    // } 
+    // float delta_time = (SDL_GetTicks() - *lastFrameTime) / 1000.0f;
+    
+    *lastFrameTime = SDL_GetTicks(); // Set the lastFrameTime to the current time.
+}
+
+void render(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);      // clear the window to the draw color
+
+
+    SDL_RenderPresent(renderer);    // draw the new image to the window
+
+}
+
+void destroy_window(SDL_Window *window, SDL_Renderer *renderer) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main() {
+    // variables required for basic SDL setup and loop
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    bool gameIsRunning = false;
+    // int lastFrameTime = 0;
+
+    gameIsRunning = initialise_window(&window, &renderer);
+
+    MenuState state = MenuState();
+    state.onEnter(window, renderer);
+
+    while (gameIsRunning) {
+        process_input(&gameIsRunning);
+        state.update();
+        state.render(renderer);
+    }
+    
+    state.onExit();
+    destroy_window(window, renderer);
+    return 0;
+}
