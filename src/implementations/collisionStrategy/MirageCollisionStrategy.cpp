7add1c8c25401fd345f6f9165f25d8cd2d54d8a7
BobHell0 <bokchoyjunior@gmail.com>
2025-02-10 23:01:00 +1100

    basically just return false for all collision checks to let the player go through the object no problem

---
+++ src/implementations/collisionStrategy/MirageCollisionStrategy.cpp
@@ -0,0 +1,13 @@
+#include "../../headerFiles/collisionStrategy/MirageCollisionStrategy.hpp"
+bool MirageCollisionStrategy::playerMovesUpCollisionCheck(ObjectPositionInfo info) {
+    return false;
+}
+bool MirageCollisionStrategy::playerMovesDownCollisionCheck(ObjectPositionInfo info) {
+    return false;
+}
+bool MirageCollisionStrategy::playerMovesLeftCollisionCheck(ObjectPositionInfo info) {
+    return false;
+}
+bool MirageCollisionStrategy::playerMovesRightCollisionCheck(ObjectPositionInfo info) {
+    return false;
+}
